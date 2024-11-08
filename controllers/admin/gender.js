c887a14dc2333a5f7e46d3b2c9d6357efa6c1970
AleGonzalez002 <denisalejandro2006@gmail.com>
2024-11-07 18:21:03 -0600

    Cambios

--- controllers/admin/gender.js
+++ controllers/admin/gender.js
@@ -1,5 +1,5 @@
 // Constantes para completar las rutas de la API.
-const GENERO_API = 'services/admin/genders.php';
+const GENERO_API = 'services/admin/gender.php';
 // Constante para establecer el formulario de buscar.
 const SEARCH_FORM = document.getElementById('searchForm');
 // Constantes para establecer el contenido de la tabla.
